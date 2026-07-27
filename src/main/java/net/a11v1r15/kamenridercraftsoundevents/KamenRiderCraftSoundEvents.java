a9239214043f18fd08b75424def46627273c963d
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2026-07-26 21:48:35 -0300

    🔖 1.0.0

---
+++ src/main/java/net/a11v1r15/kamenridercraftsoundevents/KamenRiderCraftSoundEvents.java
@@ -0,0 +1,47 @@
+package net.a11v1r15.kamenridercraftsoundevents;
+
+import java.util.function.Supplier;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(KamenRiderCraftSoundEvents.MODID)
+public class KamenRiderCraftSoundEvents {
+	// Define mod id in a common place for everything to reference
+	public static final String MODID = "kamenridercraftsoundevents";
+	// Directly reference a slf4j logger
+	public static final Logger LOGGER = LogUtils.getLogger();
+
+	public static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(BuiltInRegistries.SOUND_EVENT, KamenRiderCraftSoundEvents.MODID);
+
+	public static final Supplier<SoundEvent> HENCHMAN_AMBIENT = registerSoundEvent("entity.kamenridercraft.henchman.ambient");
+	public static final Supplier<SoundEvent> HENCHMAN_HURT = registerSoundEvent("entity.kamenridercraft.henchman.hurt");
+	public static final Supplier<SoundEvent> HENCHMAN_DEATH = registerSoundEvent("entity.kamenridercraft.henchman.death");
+	public static final Supplier<SoundEvent> HENCHMAN_STEP = registerSoundEvent("entity.kamenridercraft.henchman.step");
+	public static final Supplier<SoundEvent> HENCHMAN_SHOOT = registerSoundEvent("entity.kamenridercraft.henchman.shoot");
+
+	private static Supplier<SoundEvent> registerSoundEvent(String name) {
+		ResourceLocation id = ResourceLocation.fromNamespaceAndPath(KamenRiderCraftSoundEvents.MODID, name);
+		return SOUND_EVENTS.register(name, () -> SoundEvent.createVariableRangeEvent(id));
+	}
+
+	public static void register(IEventBus eventBus) {
+		SOUND_EVENTS.register(eventBus);
+	}
+
+	// The constructor for the mod class is the first code that is run when your mod is loaded.
+	// FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
+	public KamenRiderCraftSoundEvents(IEventBus modEventBus, ModContainer modContainer) {
+		register(modEventBus);
+	}
+}
---
+++ src/main/java/net/a11v1r15/kamenridercraftsoundevents/mixin/BaseHenchmenEntityMixin.java
@@ -0,0 +1,47 @@
+package net.a11v1r15.kamenridercraftsoundevents.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import com.kelco.kamenridercraft.entity.mobs.foot_soldiers.BaseHenchmenEntity;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+
+import net.a11v1r15.kamenridercraftsoundevents.KamenRiderCraftSoundEvents;
+import net.minecraft.sounds.SoundEvent;
+
+@Mixin(value = BaseHenchmenEntity.class)
+public abstract class BaseHenchmenEntityMixin {
+    @ModifyReturnValue(at = @At("RETURN"),
+	method = "getAmbientSound()Lnet/minecraft/sounds/SoundEvent;")
+	private SoundEvent kamenridercraftsoundevents$changeAmbientSound(SoundEvent original) {
+		return KamenRiderCraftSoundEvents.HENCHMAN_AMBIENT.get();
+	}
+	
+    @ModifyReturnValue(at = @At("RETURN"),
+	method = "getHurtSound(Lnet/minecraft/world/damagesource/DamageSource;)Lnet/minecraft/sounds/SoundEvent;")
+	private SoundEvent kamenridercraftsoundevents$changeHurtSound(SoundEvent original) {
+		return KamenRiderCraftSoundEvents.HENCHMAN_HURT.get();
+	}
+	
+    @ModifyReturnValue(at = @At("RETURN"),
+	method = "getDeathSound()Lnet/minecraft/sounds/SoundEvent;")
+	private SoundEvent kamenridercraftsoundevents$changeDeathSound(SoundEvent original) {
+		return KamenRiderCraftSoundEvents.HENCHMAN_DEATH.get();
+	}
+	
+    @ModifyReturnValue(at = @At("RETURN"),
+	method = "getStepSound()Lnet/minecraft/sounds/SoundEvent;")
+	private SoundEvent kamenridercraftsoundevents$changeStepSound(SoundEvent original) {
+		return KamenRiderCraftSoundEvents.HENCHMAN_STEP.get();
+	}
+
+	@ModifyArg( at = @At(
+		value = "INVOKE",
+		target = "playSound(Lnet/minecraft/sounds/SoundEvent;FF)V"
+	), index = 0,
+	method = "performRangedAttack(Lnet/minecraft/world/entity/LivingEntity;F)V")
+	private SoundEvent kamenridercraftsoundevents$changeShootSound(SoundEvent original) {
+		return KamenRiderCraftSoundEvents.HENCHMAN_SHOOT.get();
+	}
+}
